bb79fa63d1b4628c9b3a4c034a310fef82a94a0c
IZZAH110 <99550511+IZZAH110@users.noreply.github.com>
2022-02-14 10:49:29 +0800

    Update Next Assessment.py

--- Next Assessment.py
+++ Next Assessment.py
@@ -2,62 +2,30 @@
 # coding: utf-8
 
 # In[1]:
-
-
 import pandas as pd
 import plotly.express as px
-
-
 # In[2]:
-
-
 url = 'http://api.open-notify.org/iss-now.json'
-
-
 # In[3]:
-
-
 df = pd.read_json(url)
-
-
 # In[4]:
-
-
 df
-
-
 # In[5]:
-
-
 df ['latitude'] = df.loc['latitude'.'iss_position']
 df['longitude'] = df.loc['longitude','iss_position']
 df.reser_index(inppplace=True)
-
-
 # In[6]:
-
-
 df ['latitude'] = df.loc['latitude','iss_position']
 df['longitude'] = df.loc['longitude','iss_position']
 df.reser_index(inppplace=True)
-
-
 # In[7]:
-
-
 df ['latitude'] = df.loc['latitude','iss_position']
 df['longitude'] = df.loc['longitude','iss_position']
 df.reser_index(inplace=True)
-
-
 # In[8]:
-
-
 df ['latitude'] = df.loc['latitude','iss_position']
 df['longitude'] = df.loc['longitude','iss_position']
 df.reset_index(inplace=True)
-
-
 # In[9]:
 
 
